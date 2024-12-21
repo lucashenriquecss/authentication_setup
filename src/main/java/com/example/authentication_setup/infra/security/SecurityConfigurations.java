52b78f1458b31790ca8ca7f6c42e325cff2ce824
Lucas Henrique <43302698+lucashenriquecss@users.noreply.github.com>
2024-12-21 18:16:51 -0300

    chore: initialize Spring Security configuration
    - Create SecurityConfiguration

---
+++ src/main/java/com/example/authentication_setup/infra/security/SecurityConfigurations.java
@@ -0,0 +1,15 @@
+package com.example.authentication_setup.infra.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+    
+}
+
+/* 
+ *  EN - Disabling default Spring Security settings and declaring your own settings
+ *  PT-BR - Desabilitando configurações padrões do Spring Security e declarando configurações proprias  
+*/
