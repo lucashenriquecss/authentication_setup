722867cb77de78bc9d4ec11c409d383e93317fc3
Lucas Henrique <43302698+lucashenriquecss@users.noreply.github.com>
2024-12-21 15:34:52 -0300

    Initial commit

---
+++ src/main/java/com/example/authentication_setup/AuthenticationSetupApplication.java
@@ -0,0 +1,13 @@
+package com.example.authentication_setup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationSetupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/authentication_setup/AuthenticationSetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authentication_setup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationSetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
